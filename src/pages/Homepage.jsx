d9b263d1b9a3cc5f8bc531b8038732afebd10d18
Ahmed-469 <ahmed3almanoo@gmail.com>
2026-04-19 16:48:07 +0300

    restaurant-card-component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,34 +1,7 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
 import { useNavigate } from "react-router"
 
 function HomePage({ user }) {
-  const [restaurants, setRestaurants] = useState([])
   const navigate = useNavigate()
-
-  async function getAllRestaurants() {
-    try {
-      const getRestaurants = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/restaurants`)
-      setRestaurants(getRestaurants.data)
-    }
-    catch (err) {
-      console.log(err)
-    }
-  }
-
-  useEffect(() => {
-    getAllRestaurants()
-  }, [])
-
-  function handleRandom() {
-    if (restaurants.length === 0) return
-
-    const randomIndex = Math.floor(Math.random() * restaurants.length)
-    const randomRestaurant = restaurants[randomIndex]
-
-    navigate(`/restaurants/${randomRestaurant._id}`)
-    
-  }
 
   return (
     <div>
@@ -37,8 +10,6 @@ function HomePage({ user }) {
       <p>Find the best restaurants, explore their menus, and enjoy great food 🍔</p>
 
       <button onClick={() => navigate("/restaurants")}>Browse Restaurants</button>
-
-      <button onClick={handleRandom}> Random Restaurant </button>
 
       {!user && (
         <div>
