a8985e5a2785ec4bcc2146164bde4a28373c0c0f
Saraalsobah <salsobah2006@gmail.com>
2026-04-19 15:34:27 +0300

    Added Resturant Form + Menu Items Form

--- src/pages/RestaurantFormPage.jsx
+++ src/pages/RestaurantFormPage.jsx
@@ -1,8 +1,91 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router'
+
 
 function RestaurantFormPage() {
+
+  const { restaurantId } = useParams()
+  const navigate = useNavigate()
+
+  const [formData, setFormData] = useState({
+    name: '',
+    cuisine: '',
+    location: '',
+    logourl: '',
+  })
+
+  const fetchRestaurant = async () => {
+  try {
+    const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/restaurants/${restaurantId}`)
+    setFormData(res.data)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+  useEffect(() => {
+    if (restaurantId) {
+      fetchRestaurant()
+    }
+    else{
+      setFormData({
+      name: '',
+      cuisine: '',
+      location: '',
+      logourl: '',
+    })
+    }
+  }, [restaurantId])
+
+
+
+    function handleChange(event){
+        setFormData({...formData, [event.target.name]:event.target.value})
+    }
+
+    async function handleSubmit(event){
+    event.preventDefault()
+
+    try {
+      const token = localStorage.getItem('token')
+
+      if (restaurantId) {
+        await axios.put(`${import.meta.env.VITE_BACKEND_URL}/restaurants/${restaurantId}`,formData,{ headers: { Authorization: `Bearer ${token}`} })
+      } 
+      else {
+        await axios.post(`${import.meta.env.VITE_BACKEND_URL}/restaurants`, formData, { headers: { Authorization: `Bearer ${token}`} })
+      }
+
+      navigate('/restaurants')
+    } 
+    catch(err){
+      console.log(err)
+    }
+  }     
+    
+
   return (
-    <div>RestaurantFormPage</div>
+    <div>
+       <h1>{restaurantId ? 'Edit Restaurant' : 'Add a Restaurant'}</h1>
+      <form onSubmit={handleSubmit}>
+
+        <label htmlFor="name">Restaurant Name:</label>
+        <input name="name" value={formData.name} onChange={handleChange} placeholder="Restaurant Name"/>
+
+        <label htmlFor="cuisine">Cuisine:</label>
+        <input name="cuisine" value={formData.cuisine} onChange={handleChange} placeholder="Cuisine" />
+
+        <label htmlFor="location">Location:</label>     
+        <input name="location" value={formData.location} onChange={handleChange} placeholder="Location"/>
+
+        <label htmlFor="logourl">Logo URL:</label> 
+        <input name="logourl" value={formData.logourl} onChange={handleChange} placeholder="Logo URL"/>
+
+        <button type="submit">{restaurantId ? 'Save Changes' : 'Create Restaurant'}</button>
+        <button type="button" onClick={() => navigate('/restaurants')}>Cancel</button>
+      </form>
+    </div>
   )
 }
 
