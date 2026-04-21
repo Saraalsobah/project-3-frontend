dd51b481258f83d4015c915159de7b413a248c9a
Saraalsobah <salsobah2006@gmail.com>
2026-04-21 15:48:42 +0300

    added myRestaurant page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,6 +18,7 @@ function Navbar({ user, setUser }) {
         // Links for protected routes only for logged in users
         <>
         <Link className='nav-item' to='/restaurants/new'>Add Restaurant</Link>
+        <Link className='nav-item' to='/my-restaurants'>My Restaurants</Link>
         <span className='nav-item'>{user.username}</span>
        
         <button className='nav-item' onClick={logOut}>Log Out</button>
